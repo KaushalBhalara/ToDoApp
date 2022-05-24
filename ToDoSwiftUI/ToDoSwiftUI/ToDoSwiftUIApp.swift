236c56aff7035dac49f24b2ba7caa88b83bd5b17
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-05-24 15:38:53 +0530

    Intial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Kaushal Bhalara on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
