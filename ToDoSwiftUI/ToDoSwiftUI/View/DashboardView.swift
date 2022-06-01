ba5a7182b342f623d9fd110584e07f135d2f23b3
Kaushal Bhalara <kaushal@webmynesystems.com>
2022-06-01 18:29:35 +0530

    Working on SearchView

--- ToDoSwiftUI/ToDoSwiftUI/View/DashboardView.swift
+++ ToDoSwiftUI/ToDoSwiftUI/View/DashboardView.swift
@@ -11,14 +11,34 @@ struct DashboardView: View {
     
     let user: AppUser
     
+    @State var searchText: String = ""
+    @ObservedObject var viewModel = TODOViewModel()
+    
     var body: some View {
-        VStack{
-            Text("Dashbaord \(user.email)")
-            
-            Button{
-                AuthViewModel.shared.signout()
-            }label: {
-                Text("Sign Out")
+        ZStack
+        {
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("- All Done -")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 24))
+                    Spacer()
+                    
+                }
+                .overlay{
+                    HStack{
+                        Spacer()
+                        Button{
+                            AuthViewModel.shared.signout()
+                        }label: {
+                            Text("Logout")
+                                .foregroundColor(Color(.systemGray))
+                        }
+                        .padding(.trailing)
+                    }
+                }
+                SearchBarView(searchText: $searchText)
             }
         }
     }
---
+++ ToDoSwiftUI/ToDoSwiftUI/View/Utils/SearchBarView.swift
@@ -0,0 +1,75 @@
+//
+//  SearchBarView.swift
+//  ToDoSwiftUI
+//
+//  Created by Kaushal Bhalara on 01/06/22.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText : String
+    @State var isSeaching = false
+    
+    var body: some View {
+       
+        HStack{
+            TextField("Search TODO",text: $searchText)
+                .seachTextfieldStyle()
+                .onTapGesture(perform: {
+                    isSeaching = true
+                })
+                .overlay(
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("lightBlue"))
+                        Spacer()
+                        
+                        if isSeaching {
+                            Button{
+                                searchText = ""
+                            }label: {
+                                Image(systemName: "xmark.circle.fill")
+                            }
+                        }
+                    }
+                        .padding(.horizontal,8)
+                        .foregroundColor(.gray)
+                )
+                .transition(.move(edge: .trailing))
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ZStack{
+            BackgroundGradiantView()
+            SearchBarView(searchText: .constant(""))
+        }
+        
+    }
+}
+
+
+struct SearchTextFiledModier :ViewModifier{
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 17))
+            .padding(.leading,24)
+            .padding(.horizontal)
+            .frame(height: 45)
+            .background(Color(.systemGray6))
+            .cornerRadius(15)
+    }
+}
+
+
+extension View {
+    func seachTextfieldStyle() -> some View{
+        modifier(SearchTextFiledModier())
+    }
+}
